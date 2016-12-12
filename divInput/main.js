e5a51a865df96f061b4c1e12e716088fc3125298
tangxiaolang <1448530126@qq.com>
2016-12-12 21:55:05 +0800

    闭包

--- divInput/main.js
+++ divInput/main.js
@@ -1,6 +1,8 @@
-function removeHTMLTag(str) {
-    str = str.replace(/<\/?[^>]*>/g, ''); //去除HTML tag
-    str = str.replace(/[ | ]*\n/g, ''); //去除行尾空白
-    str = str.replace(/ /ig, ''); //去掉 
-    return str;
-}
+(function() {
+    function removeHTMLTag(str) {
+        str = str.replace(/<\/?[^>]*>/g, ''); //去除HTML tag
+        str = str.replace(/[ | ]*\n/g, ''); //去除行尾空白
+        str = str.replace(/ /ig, ''); //去掉 
+        return str;
+    }
+})();
