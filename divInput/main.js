1c6fb7ac4745df8acf04e167b9d158896829d8e7
tangxiaolang <1448530126@qq.com>
2016-12-12 21:47:56 +0800

    input div

---
+++ divInput/main.js
@@ -0,0 +1,6 @@
+function removeHTMLTag(str) {
+    str = str.replace(/<\/?[^>]*>/g, ''); //去除HTML tag
+    str = str.replace(/[ | ]*\n/g, ''); //去除行尾空白
+    str = str.replace(/ /ig, ''); //去掉 
+    return str;
+}
